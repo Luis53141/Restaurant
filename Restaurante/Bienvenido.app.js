5da7b2034b90f2ec282c2b37a99bac3879de4445
Luis53141 <73140868@idexpasco.edu.pe>
2025-04-16 14:01:36 -0500

    Delete Restaurante directory

--- Restaurante/Bienvenido.app.js
+++
@@ -1,5 +0,0 @@
-// Aquí puedes agregar tu código JavaScript
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("Bienvenido al Restaurante Norkys");
-    // Añade más funcionalidades aquí
-});
