bc990c0b5c0069f32cfa7a4b43d2e95e21b54c70
Luis53141 <73140868@idexpasco.edu.pe>
2025-04-16 13:55:20 -0500

    Add files via upload

---
+++ Restaurante/Bienvenido.app.js
@@ -0,0 +1,5 @@
+// Aquí puedes agregar tu código JavaScript
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Bienvenido al Restaurante Norkys");
+    // Añade más funcionalidades aquí
+});
